398e3cdf5ab5d1bbc49bc8be1ba938f946a0c54f
Amol Prakash <amolprakash06@gmail.com>
2020-04-02 05:14:59 +0530

    Added extensions, api and error handling classes.

---
+++ Gojek Demo/Gojek Demo/Reusable Components/Extensions/UIView+Ext.swift
@@ -0,0 +1,43 @@
+//
+//  UIView+Ext.swift
+//  Gojek Demo
+//
+//  Created by Amol Prakash on 02/04/20.
+//  Copyright © 2020 Amol Prakash. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView {
+    static var reuseIdentifier: String { get }
+}
+
+extension ReusableView {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableView {
+    func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T where T: ReusableView {
+        guard let cell = self.dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Unable to Dequeue Reusable Table View Cell")
+        }
+        return cell
+    }
+}
+
+typealias UIAlertControllerHandler = ( _ alertController: UIAlertController, _ selectedIndex: Int ) -> Void
+
+extension UIViewController {
+    func showAlert(title: String, message: String, buttonTitles: [String] = ["OK"], handler: UIAlertControllerHandler?) {
+        let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let actionHandler = { action  -> Void in
+            handler?( controller, controller.actions.firstIndex(of: action ) ?? -1 )
+        }
+        for title in buttonTitles[ 0..<buttonTitles.count ] {
+            controller.addAction( UIAlertAction( title: title, style: .default, handler: actionHandler))
+        }
+        self.present(controller, animated: true, completion: nil)
+    }
+}
---
+++ Gojek Demo/Gojek Demo/Reusable Components/Service/APIClient.swift
@@ -0,0 +1,85 @@
+//
+//  APIClient.swift
+//  Gojek Demo
+//
+//  Created by Amol Prakash on 02/04/20.
+//  Copyright © 2020 Amol Prakash. All rights reserved.
+//
+
+import Foundation
+
+class APIClient<T: Decodable> {
+    
+    public typealias Json = [String : AnyHashable]
+    public typealias Completion = (_ response :T?, _ error: CustomError?) -> Void
+    
+    static func clientRequest(with url: String,
+                              params: Json? = nil,
+                              type: HTTPMethodType = .get,
+                              completionHandler: @escaping Completion)
+    {
+        guard Reachability.isConnectedToNetwork() else {
+            completionHandler(nil, .noInternet)
+            return
+        }
+        
+        guard let url = URL(string: url) else {
+            completionHandler(nil, .badUrlRequest)
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = type.rawValue
+        request.addValue(ContentType.applicationJson,
+                         forHTTPHeaderField: HTTPHeaderField.contentType)
+        
+        if (type == .post || type == .put), let params = params {
+            do {
+                let body = try JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
+                request.httpBody = body
+            } catch {
+                completionHandler(nil, .badParamRequest)
+            }
+        }
+        
+        let task: URLSessionDataTask = URLSession.shared.dataTask(with: request) {
+            (data: Data?, resp: URLResponse?, err: Error?) in
+            DispatchQueue.main.async {
+                if let _ = err {
+                    completionHandler(nil, .serverError)
+                } else {
+                    guard let data = data, let response = try? JSONDecoder().decode(T.self, from: data) else {
+                        completionHandler(nil, .badResponse)
+                        return
+                    }
+                    completionHandler(response, nil)
+                }
+            }
+        }
+        task.resume()
+    }
+}
+
+enum HTTPMethodType : String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    
+    init(type: String) {
+        guard let type = HTTPMethodType(rawValue: type) else {
+            fatalError("Invalid MethodType, Please implement this type")
+        }
+        self = type
+    }
+}
+
+enum HTTPHeaderField {
+    static let contentType = "Content-Type"
+}
+
+enum ContentType {
+    static let applicationJson = "application/json"
+}
+
+enum EnvironmentURL {
+    static let baseUrl = "http://gojek-contacts-app.herokuapp.com/"
+}
