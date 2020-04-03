cdef5681c13910f65aab43815b74882f4d8d8904
Amol Prakash <amolprakash06@gmail.com>
2020-04-03 05:55:02 +0530

    Added contact edit screen files.

---
+++ Gojek Demo/Gojek Demo/Contact Edit Screen/View/ContactEditViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ContactEditViewController.swift
+//  Gojek Demo
+//
+//  Created by Amol Prakash on 03/04/20.
+//  Copyright © 2020 Amol Prakash. All rights reserved.
+//
+
+import UIKit
+
+class ContactEditViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    deinit {
+        print("deinit \(String(describing: self))")
+    }
+}
